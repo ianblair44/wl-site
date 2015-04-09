d5157805b6ba1a350a6e8a79c9920a019e5bcd78
Ian Blair <ian@blairventures.com>
2015-04-08 23:25:09 -0700

    Express Server Created

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var express = require('express')
-var serveStatic = require('serve-static')
+var express = require('express');
+var app = express();
 
-var app = express()
+app.use('/', express.static(__dirname + '/public'));
 
-app.use(serveStatic('/', {'index': ['default.html', 'default.htm']}))
-app.listen(3000) 
+app.listen(process.env.PORT || 3000);
